607880ac7107a15e0e17a69cd9687e09ec5dd7bb
谢正华“ <1803493121@qq.com>
2022-04-27 16:36:36 +0800

    打包完成

---
+++ dist/assets/redirect.13eb0cea.js
@@ -0,0 +1,1 @@
+import{y as n,C as c,p,K as u,o as _}from"./index.571ac389.js";const h=n({setup(i){const{currentRoute:t,replace:r}=u(),{params:a,query:o}=c(t),{path:e}=a,s=Array.isArray(e)?e.join("/"):e;return r({path:"/"+s,query:o}),(m,f)=>(_(),p("div"))}});export{h as default};
